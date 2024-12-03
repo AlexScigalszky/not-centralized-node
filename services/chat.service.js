c88e1b6fa348edb8a037a4e89bb4a9a92a3f252a
Alex P. Scigalszky <Alex P. Scigalszky>
2024-12-03 19:00:45 -0300

    generate unique id

--- services/chat.service.js
+++ services/chat.service.js
@@ -1,9 +1,11 @@
 import fetch from 'node-fetch';
+import { v4 as uuidv4 } from 'uuid';
 import { listOtherNodes } from '../repository/in-memory.js';
 import { alreadyReceived, saveMessage, listMessages } from '../repository/chat.js';
 
 // Sincronizar nodos
 export async function sendMessage(id, sender, message) {
+    id = id ? id : uuidv4();
     // Chequear que no sea un duplicado
     if (alreadyReceived(id)) {
         console.log(`Duplicado: [${id} - ${sender}]: ${message}`);
@@ -13,7 +15,7 @@ export async function sendMessage(id, sender, message) {
 
     // Guardar el mensaje localmente
     saveMessage(id, sender, message);
-    
+
     // Reenviar el mensaje a otros nodos conocidos
     listOtherNodes().forEach(async (nodeUrl) => {
         try {
